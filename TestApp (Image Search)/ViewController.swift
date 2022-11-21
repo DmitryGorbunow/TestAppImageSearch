c97495df139c471698603d19b2da9297b97ecac5
DmitryGorbunow <84672242+DmitryGorbunow@users.noreply.github.com>
2022-11-21 18:32:41 +0500

    add Networking and ImageCollectionView

--- TestApp (Image Search)/ViewController.swift
+++ TestApp (Image Search)/ViewController.swift
@@ -7,13 +7,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
+
+    struct APIResponse: Codable {
+        let images_results: [Result]
+    }
+    
+    struct Result: Codable {
+        let original: String
+    }
+    
+    let urlString = "https://serpapi.com/search.json?q=Nature&tbm=isch&ijn=0&api_key=3ba660749854382407b0fceb47b6062cfe242ddc597aeeb4f5aa597eec34b6ff"
+    
+    private var collectionView: UICollectionView?
+    
+    var results: [Result] = []
+    
+    let searchBar = UISearchBar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(searchBar)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.itemSize = CGSize(width: view.frame.size.width/2, height: view.frame.size.width/2)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: ImageCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        self.collectionView = collectionView
+        view.addSubview(collectionView)
+        fetchPhotos()
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds
+    }
+    
+    func fetchPhotos() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let jsonResults = try JSONDecoder().decode(APIResponse.self, from: data)
+                DispatchQueue.main.async {
+                    self?.results = jsonResults.images_results
+                    self?.collectionView?.reloadData()
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let imageURlString = results[indexPath.row].original
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.identifier, for: indexPath) as? ImageCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(with: imageURlString)
+        return cell
+    }
 
 }
 
